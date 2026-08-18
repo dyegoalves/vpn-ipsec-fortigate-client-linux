d4610e40053903a149c841fe6da0489ea11591d1
Dyego Alves <dyego.santos.alves@gmail.com>
2026-08-18 16:14:52 -0300

    build: bump packaging version to 0.8.1

--- packaging/appimage/build.sh
+++ packaging/appimage/build.sh
@@ -7,7 +7,7 @@ set -e  # Sair se qualquer comando falhar
 
 # Definir variáveis
 APP_NAME="VPN-IPsec-Client"
-APP_VERSION="0.8.0"
+APP_VERSION="0.8.1"
 APP_DIR="AppDir"
 APPIMAGE_NAME="${APP_NAME}-${APP_VERSION}-x86_64.AppImage"
 BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
